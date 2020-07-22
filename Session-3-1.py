84a88d72b24fc81751009ad436dbb6c083ca2f84
Mesmaeeli <63573547+Mesmaeeli@users.noreply.github.com>
2020-07-22 23:51:19 +0430

    Create Session-3-1.py

---
+++ Session-3-1.py
@@ -0,0 +1,1609 @@
+>>> print('hi')
+hi
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+hi
+>>> Num = 10
+>>> if Num > 0:
+	print('positive')
+
+	
+positive
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+positive
+Done
+kharej az body if
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+kharej az body if
+>>> Num > 0
+False
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+positive
+Done
+kharej az body if
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+kharej az body if
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+kharej az body if
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 12
+positive
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: -10
+negative
+done
+>>> 12 % 2
+0
+>>> 12 % 2 == 0
+True
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 13
+odd
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 18
+even
+Done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 15
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 14
+positive
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: -3
+negative
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 15
+positive
+negative
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: -14
+negative
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+>>> 
+yek adad vared konid: 0
+zero
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 15
+positive
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 139
+bozorgtar az 100
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 56
+beyn 0 o 100
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: -9
+manfi
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 139
+bozorgtar az 100
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 120
+bozorgtar az 100
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 
+Traceback (most recent call last):
+  File "C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py", line 1, in <module>
+    Num = int(input('yek adad vared konid: '))
+KeyboardInterrupt
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+passed
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+passed
+17.75
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+failed
+5.0
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 12
+bakhshpazir bar 6
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 12
+adad mored naza ma
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 4
+adad mored naza ma
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 9
+adad mored naza ma
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 9
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 4
+adad mored naza ma
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+sal miladi vared konid200
+Traceback (most recent call last):
+  File "C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py", line 81, in <module>
+    num = int(input ( "sal miladi vared konid"))
+KeyboardInterrupt
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+sal miladi vared konid: 2000
+kabise
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+sal miladi vared konid: 2002
+gheir kabise
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+sal miladi vared konid: 2100
+gheir kabise
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+sal miladi vared konid: 2001
+gheir kabise
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+please input  year milade :2000
+hast
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+please input  year milade :2020
+hast
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+please input  year milade :2100
+nist
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+please input  year milade :2003
+nist
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+5
+Traceback (most recent call last):
+  File "C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py", line 90, in <module>
+    print(Num)
+KeyboardInterrupt
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 5
+5
+4
+3
+2
+1
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 60
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+>>> l1 = [3,5,6\
+	  ,8,5,9]
+>>> l1
+[3, 5, 6, 8, 5, 9]
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 91
+91
+92
+93
+94
+95
+96
+97
+98
+99
+>>> l1
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    l1
+NameError: name 'l1' is not defined
+>>> 
+=============================== RESTART: Shell ===============================
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 90
+90
+92
+94
+96
+98
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 97
+Traceback (most recent call last):
+  File "C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py", line 100, in <module>
+    if Num % 2 == 0:
+KeyboardInterrupt
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 60
+>>> print(l1)
+[60, 65, 70, 75, 80, 85, 90, 95]
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+85
+12
+86
+123
+56
+12
+85
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+85
+12
+86
+123
+56
+12
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+19
+16
+14
+12
+7
+20
+passed
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+19
+16
+14
+12
+7
+20
+passed
+14.666666666666666
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+19
+16
+14
+12
+7
+0
+failed
+11.333333333333334
+>>> l1 = [12,52,63,8,14,73,90]
+>>> sum(l1)
+312
+>>> l1[:4]
+[12, 52, 63, 8]
+>>> sum(l1[:4])
+135
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 12
+12
+17
+22
+27
+32
+37
+42
+47
+52
+57
+62
+67
+72
+77
+82
+87
+92
+97
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+145
+6
+14
+90
+>>> l1
+[45, 2, 7, 145, 6, 14, 90]
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+145
+6
+14
+90
+M
+o
+h
+a
+m
+m
+a
+d
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+145
+6
+14
+90
+M
+o
+h
+a
+m
+m
+a
+d
+1
+5
+12
+4
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+145
+6
+14
+90
+M
+o
+h
+a
+m
+m
+a
+d
+1
+5
+12
+4
+1
+2
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+1
+2
+>>> 1 in dict1
+True
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+7
+6
+90
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+7
+6
+90
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+M mohammad
+o mohammad
+h mohammad
+a mohammad
+m mohammad
+m mohammad
+a mohammad
+d mohammad
+>>> str1
+'Mohammad'
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+Traceback (most recent call last):
+  File "C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py", line 160, in <module>
+    if l1 % 2 == 0:
+TypeError: unsupported operand type(s) for %: 'list' and 'int'
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+even
+even
+even
+even
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+2
+even
+6
+even
+14
+even
+90
+even
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+Traceback (most recent call last):
+  File "C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py", line 168, in <module>
+    for i in num:
+TypeError: 'int' object is not iterable
+>>> l1
+[45, 2, 7, 145, 6, 14, 90]
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+145
+6
+14
+90
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+>>> type(l2)
+<class 'list'>
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+6
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+s
+a
+l
+a
+m
+>>> range(10)
+range(0, 10)
+>>> range(3,10)
+range(3, 10)
+>>> range(0,100,10)
+range(0, 100, 10)
+>>> for i in range(10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> for i in range(3,10):
+	print(i)
+
+	
+3
+4
+5
+6
+7
+8
+9
+>>> for i in range(0,100,10):
+	print(i)
+
+	
+0
+10
+20
+30
+40
+50
+60
+70
+80
+90
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+145
+6
+14
+90
+>>> l1
+[45, 2, 7, 145, 6, 14, 90]
+>>> NUM
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    NUM
+NameError: name 'NUM' is not defined
+>>> Num
+7
+>>> a = range(10)
+>>> type(a)
+<class 'range'>
+>>> a
+range(0, 10)
+>>> b = list(a)
+>>> b
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> range(0,10,0.5)
+Traceback (most recent call last):
+  File "<pyshell#47>", line 1, in <module>
+    range(0,10,0.5)
+TypeError: 'float' object cannot be interpreted as an integer
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+1
+2
+3
+4
+5
+6
+7
+8
+9
+2
+4
+6
+8
+10
+12
+14
+16
+18
+3
+6
+9
+12
+15
+18
+21
+24
+27
+4
+8
+12
+16
+20
+24
+28
+32
+36
+5
+10
+15
+20
+25
+30
+35
+40
+45
+6
+12
+18
+24
+30
+36
+42
+48
+54
+7
+14
+21
+28
+35
+42
+49
+56
+63
+8
+16
+24
+32
+40
+48
+56
+64
+72
+9
+18
+27
+36
+45
+54
+63
+72
+81
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+1
+2
+2
+4
+>>> print(l1,'salam',123)
+[45, 2, 7, 145, 6, 14, 90] salam 123
+>>> l1
+[45, 2, 7, 145, 6, 14, 90]
+>>> fo i in reversed(l1):
+	
+SyntaxError: invalid syntax
+>>> for i in reversed(l1):
+	print(i)
+
+	
+90
+14
+6
+145
+7
+2
+45
+>>> a1 = reversed(l1)
+>>> type(a1)
+<class 'list_reverseiterator'>
+>>> l1
+[45, 2, 7, 145, 6, 14, 90]
+>>> l1.reverse()
+>>> l1
+[90, 14, 6, 145, 7, 2, 45]
+>>> l2 = [8,5,13,442,12,4,12]
+>>> l1
+[90, 14, 6, 145, 7, 2, 45]
+>>> l2
+[8, 5, 13, 442, 12, 4, 12]
+>>> for i in zip(l1,l2):
+	print(i)
+
+	
+(90, 8)
+(14, 5)
+(6, 13)
+(145, 442)
+(7, 12)
+(2, 4)
+(45, 12)
+>>> a1 = zip(l1,l2)
+>>> type(a1)
+<class 'zip'>
+>>> l3 = [6,2,7,1,12,13]
+>>> for i in zip(l1,l2,l3):
+	print(i)
+
+	
+(90, 8, 6)
+(14, 5, 2)
+(6, 13, 7)
+(145, 442, 1)
+(7, 12, 12)
+(2, 4, 13)
+>>> len(l1)
+7
+>>> len(l2)
+7
+>>> len(l3)
+6
+>>> l1
+[90, 14, 6, 145, 7, 2, 45]
+>>> l2
+[8, 5, 13, 442, 12, 4, 12]
+>>> l3
+[6, 2, 7, 1, 12, 13]
+>>> a1 = zip(l1,l2,l3)
+>>> a1[0]
+Traceback (most recent call last):
+  File "<pyshell#77>", line 1, in <module>
+    a1[0]
+TypeError: 'zip' object is not subscriptable
+>>> a1 = range(10)
+>>> a1[0]
+0
+>>> str1 = 'salam'
+>>> str1**2
+Traceback (most recent call last):
+  File "<pyshell#81>", line 1, in <module>
+    str1**2
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+>>> for i in enumerate(l1):
+	print(i)
+
+	
+(0, 90)
+(1, 14)
+(2, 6)
+(3, 145)
+(4, 7)
+(5, 2)
+(6, 45)
+>>> l1
+[90, 14, 6, 145, 7, 2, 45]
+>>> l1
+[90, 14, 6, 145, 7, 2, 45]
+>>> for i in l1:
+	print(i)
+
+	
+90
+14
+6
+145
+7
+2
+45
+>>> a1 = enumerate(l1)
+>>> type(a1)
+<class 'enumerate'>
+>>> l2 = [(12,45),(56,12),(0,0),(8,9)]
+>>> for points in l2:
+	print(points)
+
+	
+(12, 45)
+(56, 12)
+(0, 0)
+(8, 9)
+>>> for points in l2:
+	print(points[0])
+
+	
+12
+56
+0
+8
+>>> for points in l2:
+	print(points[0])
+	print(points[1])
+
+	
+12
+45
+56
+12
+0
+0
+8
+9
+>>> l2
+[(12, 45), (56, 12), (0, 0), (8, 9)]
+>>> for x,y in l2:
+	print(x)
+	print(y)
+
+	
+12
+45
+56
+12
+0
+0
+8
+9
+>>> l3 = [(12, 45), (56, 12), (0, 0), (0,0,0)]
+>>> for x,y in l3:
+	print(x,y)
+
+	
+12 45
+56 12
+0 0
+Traceback (most recent call last):
+  File "<pyshell#109>", line 1, in <module>
+    for x,y in l3:
+ValueError: too many values to unpack (expected 2)
+>>> l3 = [(12, 45), (56, 12), (0, 0), 12]
+>>> for x,y in l3:
+	print(x,y)
+
+	
+12 45
+56 12
+0 0
+Traceback (most recent call last):
+  File "<pyshell#112>", line 1, in <module>
+    for x,y in l3:
+TypeError: 'int' object is not iterable
+>>> l1 = [5,3,6,8]
+>>> l2 = [6,2,6,1]
+>>> l3 = [6,2,99,0]
+>>> for i1,i2,i3 in zip(l1,l2,l3):
+	print(i1,i2,i3)
+
+	
+5 6 6
+3 2 2
+6 6 99
+8 1 0
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 85
+85
+87
+89
+91
+93
+95
+97
+99
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 85
+85
+86
+87
+88
+89
+91
+92
+93
+94
+95
+96
+97
+98
+99
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 85
+85
+86
+87
+88
+89
+91
+92
+93
+94
+95
+96
+97
+98
+99
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 85
+85
+86
+87
+88
+89
+>>> l1
+[45, 2, 7, 145, 6, 14, 90]
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+yek adad vared konid: 
+Traceback (most recent call last):
+  File "C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py", line 1, in <module>
+    Num = int(input('yek adad vared konid: '))
+KeyboardInterrupt
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+6
+14
+90
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+145
+6
+14
+90
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+145
+6
+14
+90
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+10
+9
+8
+7
+6
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+10
+9
+8
+7
+6
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+10
+9
+8
+7
+6
+4
+3
+2
+1
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+145
+6
+14
+90
+done
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+45
+2
+7
+>>> l1
+[45, 2, 7, 145, 6, 14, 90]
+>>> 
+== RESTART: C:/Users/esmae/AppData/Local/Programs/Python/Python36/p12-s3.py ==
+done
+>>> 
