34382e723d2e6b85a20991e235ad5f87fa28e92e
Mesmaeeli <63573547+Mesmaeeli@users.noreply.github.com>
2020-07-28 04:09:05 +0430

    Create Session-5-2.py

---
+++ Session-5-2.py
@@ -0,0 +1,25 @@
+"""
+age = int(input('sen khod ra vared konid: '))
+
+assert age > 0 ,'sen vared shode sahih nist'
+
+print('ali',age,'sal sen darad')
+"""
+
+"""
+class People:
+   age = 0
+   name = ''
+   id_card = 0
+
+
+person1 = People()
+person1.age = 90
+person1.name = 'ali'
+person1.id_card = 7398397
+
+attr = input('kodum attr mikhay taghir bedi ya bebini: ')
+print(getattr(person1,attr))
+print(hasattr(person1,attr))
+delattr
+"""
